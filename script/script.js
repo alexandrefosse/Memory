46ed05da67dc5db920331f7f62d57fb1f2eb53ce
Alexandre Fosse <alexandre.fosse@etu.unicaen.fr>
2026-09-20 01:59:04 +0200

    ajout de la recuperation des url duplication des url et de la fonction mélange des cartes

--- script/script.js
+++ script/script.js
@@ -1,8 +1,39 @@
-let dimention = 150 ;
+let dimension = 150 ;
 let imgStart = Math.floor(Math.random()*100)+1
 
-let images = [] ;
+let url = [] ;
 
+jeu = []
+
+/**
+ * Récuperation des images aléatoires
+ */
 for (let i = 0 ; i < 8 ; i++) {
-    images.push(`https://picsum.photo/seed/${imgStart+i}/${dimention}`)
+    url.push(`https://picsum.photos/id/${imgStart+i}/${dimension}`)
+}
+
+
+/**
+ * Duplication des cartes
+ */
+for (let i = 0 ; i < 8 ; i++) {
+    jeu[i] = url[i];
+    jeu[i + 8] = url[i];
+}
+
+
+/**
+ * Algo de Fisher-Yates
+ * Fonction de mélange
+ */
+
+function shuffle(cards = []) {
+    for (let i = cards.length-1 ; i > 0 ; i--) {
+        let rand = Math.floor(Math.random()*(i+1))
+
+        let temp = cards[rand];
+        cards[rand] = cards[i];
+        cards[i] = temp;
+    }
+    return cards;
 }
